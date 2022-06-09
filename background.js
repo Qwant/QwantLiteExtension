6646f231a3713e7a7f6041637c196a0f549fac94
Raed667 <github@raed.email>
2022-06-09 15:23:05 +0200

    only save relavent settings

--- background.js
+++ background.js
@@ -1,91 +1,99 @@
+let previousURL = null;
 
-let previousURL = null
-
-const SETTINGS_KEY = 'ext_set'
+const SETTINGS_KEY = "ext_set";
+const KEYS = ['l', 'locale', 's', 'a', 'b', 'theme', 'ta']
 
 const deleteEmpty = (obj) => {
-    Object.keys(obj).forEach(key => {
+    Object.keys(obj).forEach((key) => {
         if (obj[key] === undefined) {
             delete obj[key];
         }
     });
-    return obj
-}
+    return obj;
+};
 
 const getParams = (url) => {
-    const params = {}
-
-    const urlParams = new URLSearchParams(url.replace('https://lite.qwant.com/', '').replace('https://lite.qwant.com/settings', ''));
-
+    const params = {};
+    const urlParams = new URLSearchParams(
+        url
+            .replace("https://lite.qwant.com/", "")
+            .replace("https://lite.qwant.com/settings", "")
+    );
     const entries = urlParams.entries();
+
     for (const entry of entries) {
-        params[entry[0]] = entry[1]
+        params[entry[0]] = entry[1];
     }
 
-    return deleteEmpty(params)
-}
-
-const isSearchQuery = ({ method, type, url }) => {
-    if (method !== "GET" || type !== "main_frame") return false
-    return !!getParams(url).q
-}
+    return deleteEmpty(params);
+};
 
 const saveSettings = (url) => {
-    const settings = getParams(url)
+    const settings = getParams(url);
 
-    delete settings.q
-    delete settings['settings?']
-    delete settings['settings?q']
+    delete settings.q;
+    delete settings["settings?"];
+    delete settings["settings?q"];
 
-    browser.storage.local.set({ settings })
-    return settings
-}
+    browser.storage.local.set({
+        settings
+    });
+
+    return settings;
+};
 
 const getSearchQuery = async (url) => {
-    const { q, ...params } = getParams(url)
-    const result = {}
+    const { q, ...params } = getParams(url);
+    const { settings } = await browser.storage.local.get("settings");
 
-    const { settings } = await browser.storage.local.get("settings")
+    if (!settings || Object.keys(settings).length === 0) {
+        return null;
+    }
 
-    if (!settings || Object.keys(settings).length === 0) { return null }
-
-    Object.keys(settings).forEach(key => {
-        result[key] = settings[key]
-    })
-
-    Object.keys(params).forEach(key => {
-        result[key] = params[key]
-    })
+    const result = {};
+    Object.keys(settings).forEach((key) => {
+        if (KEYS.includes(key.toLocaleLowerCase()) && settings[key]) {
+            result[key] = settings[key];
+        }
+    });
+    Object.keys(params).forEach((key) => {
+        if (params[key]) {
+            result[key] = params[key];
+        }
+    });
 
     const query = new URLSearchParams({
         q,
         ...result,
         [SETTINGS_KEY]: "1"
-    }).toString()
+    }).toString();
 
-    return query
-}
+    return query;
+};
 
 browser.webRequest.onBeforeRequest.addListener(
     async (info) => {
-        if (info.method !== "GET" || info.type !== "main_frame") return
+        if (info.method !== "GET" || info.type !== "main_frame") return;
+        const queryParams = getParams(info.url);
 
-        const queryParams = getParams(info.url)
-
-        // User save new settings
-        if (previousURL && previousURL.startsWith('https://lite.qwant.com/settings')) {
-            saveSettings(info.url)
+        if (
+            previousURL &&
+            previousURL.startsWith("https://lite.qwant.com/settings")
+        ) {
+            saveSettings(info.url);
         } else if (!!queryParams.q && !queryParams[SETTINGS_KEY]) {
-            const searchParams = await getSearchQuery(info.url)
+            const searchParams = await getSearchQuery(info.url);
+
             if (searchParams) {
                 return {
                     redirectUrl: "https://lite.qwant.com/?" + searchParams
-                }
+                };
             }
         }
-
-        previousURL = info.url
+        previousURL = info.url;
     },
-    { urls: ["https://lite.qwant.com/*"] },
+    {
+        urls: ["https://lite.qwant.com/*"]
+    },
     ["blocking"]
 );
